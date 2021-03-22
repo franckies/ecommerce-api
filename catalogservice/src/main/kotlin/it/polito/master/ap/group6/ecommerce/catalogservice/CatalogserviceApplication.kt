9eb4bfca02dab1686450e472fdf43a83d1851b0c
Nicolò Chiapello <s291553@studenti.polito.it>
2021-03-22 14:58:25 +0100

    User database
    - defined User model
    - configured host and DB name to connect to MongoDB
    - created Repository abstraction to connect to MongoDB
    - defined roles (admin vs. customer)
    - populated the collection with dummy data
    - compliant with git practices

--- catalogservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/catalogservice/CatalogserviceApplication.kt
+++ catalogservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/catalogservice/CatalogserviceApplication.kt
@@ -1,10 +1,27 @@
 package it.polito.master.ap.group6.ecommerce.catalogservice
 
+import it.polito.master.ap.group6.ecommerce.catalogservice.models.User
+import it.polito.master.ap.group6.ecommerce.catalogservice.repositories.UserRepository
+import it.polito.master.ap.group6.ecommerce.common.misc.UserRole
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CatalogserviceApplication
+class CatalogserviceApplication(
+	userRepository: UserRepository
+) {
+	init {
+		// clear table
+		userRepository.deleteAll()
+
+		// populate User table
+		userRepository.save(User("Nicolò", "Chiapello", "nico", "123", "Corso Duca degli Abruzzi"))
+		userRepository.save(User("Francesco", "Semeraro", "fra", "456", "Headquarter K1"))
+		userRepository.save(User("Andrea", "Biondo", "andre", "789"))
+		userRepository.save(User("Raffaele", "Martone", "raffa", "741"))
+		userRepository.save(User("Govanni", "Malnati", "giova", "963", role=UserRole.ADMIN))
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CatalogserviceApplication>(*args)
---
+++ catalogservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/catalogservice/models/User.kt
@@ -0,0 +1,57 @@
+//======================================================================================================================
+//   Libraries
+//======================================================================================================================
+
+//------- package declaration --------------------------------------------------
+package it.polito.master.ap.group6.ecommerce.catalogservice.models
+
+//------- external dependencies ------------------------------------------------
+import it.polito.master.ap.group6.ecommerce.common.misc.UserRole
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+
+
+//======================================================================================================================
+//   Class
+//======================================================================================================================
+/**
+ * The user model. Describes an user with all the information needed to login.
+ * @property name First name of the user.
+ * @property surname Last name of the user.
+ * @property username Unique nickname inside the platform.
+ * @property password Login credentials.
+ * @property deliveryAddress (optional) Place to deliver the submitted orders.
+ * @property role Distinguish between standard buyers and system administrators.
+ *
+ * @author Nicolò Chiapello
+ */
+@Document("users")
+class User {
+
+    //------- attributes -------------------------------------------------------
+    @Id
+    var id: String? = null
+
+    var name: String? = null
+    var surname: String? = null
+
+    var username: String? = null
+    var password: String? = null
+
+    var deliveryAddress: String? = null
+    var role: UserRole? = null
+
+    //------- constructors -----------------------------------------------------
+    constructor(name: String, surname: String, username: String, password: String,
+                deliveryAddress: String? = null, role: UserRole = UserRole.CUSTOMER) {
+        this.name = name
+        this.surname = surname
+        this.username = username
+        this.password = password
+        this.deliveryAddress = deliveryAddress
+        this.role = role
+    }
+
+    //------- methods ----------------------------------------------------------
+}
