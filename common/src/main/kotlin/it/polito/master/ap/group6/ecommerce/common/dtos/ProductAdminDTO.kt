c03982ee5ae195ec54e19ee512823710ffe1757e
Raffaele Martone <s291540>
2021-03-25 15:58:55 +0100

    WAREHOUSE SERVICE UPDATE
    - Code aligned with new DTO definitions
    - Embedded end point responses with HTTP status

--- common/src/main/kotlin/it/polito/master/ap/group6/ecommerce/common/dtos/ProductAdminDTO.kt
+++ common/src/main/kotlin/it/polito/master/ap/group6/ecommerce/common/dtos/ProductAdminDTO.kt
@@ -3,8 +3,8 @@ package it.polito.master.ap.group6.ecommerce.common.dtos
 //needed when the admin wants to modify the product
 data class ProductAdminDTO(
     val product: ProductDTO? = null,
-    //val warehouse: WarehouseDTO? = null,
-    val warehouseID: String? = null,
+    val warehouse: WarehouseDTO? = null,
+//    val warehouseID: String? = null,
     val alarmLevel: Int? = null,
     val warehouseQuantity: Int? = null //quantity of that product in this warehouse
 )
