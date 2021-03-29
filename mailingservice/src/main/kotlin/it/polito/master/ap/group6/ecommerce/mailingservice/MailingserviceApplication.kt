98f1b9c63441ffc1cfcbbf26f98942586bb0fcc5
Nicolò Chiapello <s291553@studenti.polito.it>
2021-03-29 12:22:04 +0200

    Mailing service
    - created skeleton for mail service
    - configured to react to Kafka events

---
+++ mailingservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/mailingservice/MailingserviceApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.master.ap.group6.ecommerce.mailingservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MailingserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MailingserviceApplication>(*args)
+}
