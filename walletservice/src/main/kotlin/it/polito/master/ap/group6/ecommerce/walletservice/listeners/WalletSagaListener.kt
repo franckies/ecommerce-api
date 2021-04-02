f70d5044863b6b2ac7efcb05a83d1e68d0befe80
a.biondo <s291512@studenti.polito.it>
2021-04-02 14:34:52 +0200

    WalletServiceAsync implementation
    -Kafka listener
    -WalletSaga to create a transaction and perform a rollback.

---
+++ walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/listeners/WalletSagaListener.kt
@@ -0,0 +1,39 @@
+package it.polito.master.ap.group6.ecommerce.walletservice.listeners
+
+import it.polito.master.ap.group6.ecommerce.common.dtos.PlacedOrderDTO
+import it.polito.master.ap.group6.ecommerce.walletservice.miscellaneous.Response
+import it.polito.master.ap.group6.ecommerce.walletservice.miscellaneous.ResponseType
+import it.polito.master.ap.group6.ecommerce.walletservice.services.WalletService
+import it.polito.master.ap.group6.ecommerce.walletservice.services.WalletServiceAsync
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class WalletSagaListener(
+        val kafkaTemplate: KafkaTemplate<String, String>,
+        @Autowired private val walletServiceAsync: WalletServiceAsync,
+        @Autowired private val walletService: WalletService,
+) {
+
+    @KafkaListener(groupId = "ecommerce", topics = ["create_order"])
+    fun createTransaction(placedOrder: PlacedOrderDTO) {
+        println("WalletSagaListener.createTransaction: Received Kafka message on topic create_order with message $placedOrder")
+        val response: Response = walletServiceAsync.createTransaction(placedOrder)
+        if (response.responseId == ResponseType.USER_WALLET_TRANSACTION) {
+            kafkaTemplate.send("wallet_ok", placedOrder.sagaID.toString())
+        }
+        else {
+            kafkaTemplate.send("rollback", placedOrder.sagaID.toString())
+            println("WalletSagaListener.createTransaction: An error occurred processing the order ${placedOrder.sagaID}. The order is failed.")
+        }
+    }
+
+    @KafkaListener(groupId = "ecommerce", topics = ["rollback"])
+    fun rollbackTransaction(orderId: String) {
+        println("WalletSagaListener.rollbackTransaction: Received Kafka message on topic rollback with message $orderId")
+        val response: Response = walletService.undoTransaction(orderId)
+
+    }
+}
---
+++ walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/services/WalletServiceAsync.kt
@@ -0,0 +1,80 @@
+package it.polito.master.ap.group6.ecommerce.walletservice.services
+
+import it.polito.master.ap.group6.ecommerce.common.dtos.PlacedOrderDTO
+import it.polito.master.ap.group6.ecommerce.common.dtos.RechargeDTO
+import it.polito.master.ap.group6.ecommerce.common.dtos.TransactionDTO
+import it.polito.master.ap.group6.ecommerce.common.misc.TransactionStatus
+import it.polito.master.ap.group6.ecommerce.walletservice.miscellaneous.Response
+import it.polito.master.ap.group6.ecommerce.walletservice.models.dtos.Transaction
+import it.polito.master.ap.group6.ecommerce.walletservice.models.dtos.Wallet
+import it.polito.master.ap.group6.ecommerce.walletservice.models.dtos.toModel
+import it.polito.master.ap.group6.ecommerce.walletservice.repositories.TransactionRepository
+import it.polito.master.ap.group6.ecommerce.walletservice.repositories.WalletRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.*
+
+interface WalletServiceAsync {
+    fun createTransaction(placedOrder: PlacedOrderDTO?): Response
+}
+
+/**
+ * The order service. Implements the business logic.
+ * @param walletRepository a reference to the wallet Repository handling the database interaction.
+ * @param transactionRepository a reference to the transaction Repository handling the database interaction.
+ * @author Andrea Biondo
+ */
+@Service
+class WalletServiceAsyncImpl(
+        @Autowired private val walletRepository: WalletRepository,
+        @Autowired private val transactionRepository: TransactionRepository
+) : WalletServiceAsync {
+
+    override fun createTransaction(placedOrder: PlacedOrderDTO?): Response {
+
+        var res: Response
+
+        val amount: Float? = placedOrder?.purchaseList?.map { it.sellingPrice!! * it.quantity!!.toFloat() }?.sum()
+
+        try {
+            val wallet = walletRepository.findByUserID(placedOrder?.userID.toString())
+            val transaction = Transaction(null, placedOrder?.sagaID, placedOrder?.userID, amount, Date(), causal = "OrderID: ${placedOrder?.sagaID}", null)
+
+
+            if (wallet.total!! >= amount!!) {
+
+                transaction.status = TransactionStatus.ACCEPTED
+                val transactionSaved = transactionRepository.save(transaction)
+                wallet.total = wallet.total!! - transaction.amount!!
+                wallet.transactions.apply {
+                    this!!.add(transactionSaved)
+                }
+                walletRepository.save(wallet)
+
+                res =  Response.userWalletPostTransaction()
+                res.body = transactionSaved.id!!
+
+            }
+            else {
+
+                transaction.status = TransactionStatus.REFUSED
+                val transactionSaved = transactionRepository.save(transaction)
+                wallet.transactions.apply {
+                    this!!.add(transactionSaved)
+                }
+                walletRepository.save(wallet)
+                res =  Response.userWalletNoMoney()
+
+            }
+        }
+        catch (e:Exception) {
+
+            res =  Response.userWalletFailed()
+
+        }
+
+        return res
+
+    }
+
+}
