805ecaf2047cfde2895c72ac406a90fc0c9d70ca
Nicolò Chiapello <s291553@studenti.polito.it>
2021-04-02 16:26:38 +0200

    Asynchronous workflow
    - forgot last piece

--- catalogservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/catalogservice/controllers/OrderController.kt
+++ catalogservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/catalogservice/controllers/OrderController.kt
@@ -9,7 +9,6 @@ package it.polito.master.ap.group6.ecommerce.catalogservice.controllers
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import org.springframework.http.HttpStatus
-import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
 import javax.servlet.http.HttpServletRequest
@@ -45,18 +44,19 @@ class OrderController(
     //------- methods ----------------------------------------------------------
     /**
      * Create an order for the userID user with the details specified in PlacedOrderDTO.
+     * This exploits a synchronous workflow, with OrderService behaving as orchestrator.
      * @return the representation of the created order (with ID and status).
      * @throws HttpStatus.NOT_FOUND if the user doesn't exist or the remote microservice doesn't respond.
      */
-    @PostMapping("{userID}")
-    fun createOrder(@PathVariable("userID") userID: String,  //TODO enhance by retrieving userID by the logged credentials
+    @PostMapping("{userID}/sync")
+    fun createOrderSync(@PathVariable("userID") userID: String,  //TODO enhance by retrieving userID by the logged credentials
                     @RequestBody placedOrderDTO: PlacedOrderDTO): ResponseEntity<OrderDTO> {
         // log incoming request
         val currentRequest: HttpServletRequest? = (RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes)?.request
         logger.info { "Received POST on url='${currentRequest?.requestURL}' with body=${placedOrderDTO}" }
 
         // invoke the business logic
-        val created_order = orderService.createOrder(userID, placedOrderDTO)
+        val created_order = orderService.createOrderSync(userID, placedOrderDTO)
 
         // check the result
         return when (created_order.code) {
@@ -68,6 +68,34 @@ class OrderController(
             ExecutionResultType.PAYMENT_REFUSED -> ResponseEntity(null, HttpStatus.PAYMENT_REQUIRED)
             ExecutionResultType.WITHDRAWAL_REFUSED -> ResponseEntity(null, HttpStatus.CONFLICT)
             else -> ResponseEntity(created_order.body, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    /**
+     * Create an order for the userID user with the details specified in PlacedOrderDTO.
+     * This exploits an asynchronous workflow, with Kafka behaving as choreography broker.
+     * @return the representation of the created order (with ID and status).
+     */
+    @PostMapping("{userID}/async")
+    fun createOrderAsync(@PathVariable("userID") userID: String,  //TODO enhance by retrieving userID by the logged credentials
+                    @RequestBody placedOrderDTO: PlacedOrderDTO): ResponseEntity<Any?> {
+        // log incoming request
+        val currentRequest: HttpServletRequest? = (RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes)?.request
+        logger.info { "Received POST on url='${currentRequest?.requestURL}' with body=${placedOrderDTO}" }
+
+        // invoke the business logic
+        val empty_placeholder = orderService.createOrderAsync(userID, placedOrderDTO)
+
+        // check the result
+        return when (empty_placeholder.code) {
+            ExecutionResultType.CORRECT_EXECUTION -> ResponseEntity(empty_placeholder.body, HttpStatus.OK)
+            ExecutionResultType.GENERIC_ERROR -> ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+            ExecutionResultType.HTTP_ERROR -> ResponseEntity(null, empty_placeholder.http_code!!)
+            ExecutionResultType.EXTERNAL_HOST_NOT_REACHABLE -> ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+            ExecutionResultType.SOMEONE_ELSE_PROBLEM -> ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR)
+            ExecutionResultType.PAYMENT_REFUSED -> ResponseEntity(null, HttpStatus.PAYMENT_REQUIRED)
+            ExecutionResultType.WITHDRAWAL_REFUSED -> ResponseEntity(null, HttpStatus.CONFLICT)
+            else -> ResponseEntity(empty_placeholder.body, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 
