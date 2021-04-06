a1f83f9632153a9ef3f4f10bf2f9ae262a15bbea
a.biondo <s291512@studenti.polito.it>
2021-04-06 10:04:10 +0200

    Inserted rollback logger on WalletService

--- walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/listeners/WalletSagaListener.kt
+++ walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/listeners/WalletSagaListener.kt
@@ -3,6 +3,8 @@ package it.polito.master.ap.group6.ecommerce.walletservice.listeners
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import it.polito.master.ap.group6.ecommerce.common.dtos.PlacedOrderDTO
+import it.polito.master.ap.group6.ecommerce.common.dtos.RollbackDTO
+import it.polito.master.ap.group6.ecommerce.common.misc.MicroService
 import it.polito.master.ap.group6.ecommerce.walletservice.miscellaneous.Response
 import it.polito.master.ap.group6.ecommerce.walletservice.miscellaneous.ResponseType
 import it.polito.master.ap.group6.ecommerce.walletservice.services.WalletService
@@ -31,15 +33,16 @@ class WalletSagaListener(
             kafkaTemplate.send("wallet_ok", placedOrder.sagaID.toString())
         }
         else {
-            kafkaTemplate.send("rollback", placedOrder.sagaID.toString())
+            val message: String? = jacksonObjectMapper().writeValueAsString(RollbackDTO(placedOrder.sagaID.toString(), MicroService.WALLET_SERVICE))
+            kafkaTemplate.send("rollback", message)
             println("WalletSagaListener.createTransaction: An error occurred processing the order ${placedOrder.sagaID}. The order is failed.")
         }
     }
 
     @KafkaListener(groupId = "walletservice", topics = ["rollback"])
-    fun rollbackTransaction(orderId: String) {
-        println("WalletSagaListener.rollbackTransaction: Received Kafka message on topic rollback with message $orderId")
-        val response: Response = walletService.undoTransaction(orderId,
+    fun rollbackTransaction(rollback: RollbackDTO) {
+        println("WalletSagaListener.rollbackTransaction: Received Kafka message on topic rollback with message ${rollback.sagaID}, sent by ${rollback.sender}")
+        val response: Response = walletService.undoTransaction(rollback.sagaID,
             it.polito.master.ap.group6.ecommerce.common.misc.TransactionStatus.REFUSED)
 
     }
--- walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/repositories/TransactionRepository.kt
+++ walletservice/src/main/kotlin/it/polito/master/ap/group6/ecommerce/walletservice/repositories/TransactionRepository.kt
@@ -5,8 +5,10 @@ import it.polito.master.ap.group6.ecommerce.walletservice.models.dtos.Wallet
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
+@Transactional
 interface TransactionRepository: MongoRepository<Transaction, ObjectId> {
     fun findById(id: String): Transaction
     fun findByOrderID(id: String): Transaction
